bf6c4c1a0cab648f04be97a45bdab46b4dadfb05
TonyBermea <tony.bermea@icloud.com>
2023-02-22 10:57:09 -0800

    New API key

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,11 +1,5 @@
-/*
-
-bac84e7e4427b823805adc135df13262
-
-*/
-
 const url = 'https://api.openweathermap.org/data/2.5/'
-const key = '739ef515e3ade2c79525a82f51f458a5'
+const key = 'bac84e7e4427b823805adc135df13262'
 const cityName = 'Vancouver'
 
 $.getJSON(`${url}weather?q=${cityName}&appid=${key}&units=metric&lang=en`, function(data) {
