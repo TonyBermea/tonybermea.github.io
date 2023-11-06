99d9c3f8c3809b4b5cfd25dbe7ddf7f54b38c16c
tonybermea <tony.bermea@icloud.com>
2023-11-06 14:35:53 -0800

    Phone Upade and Hidden CV

--- assets/js/tippy.js
+++ assets/js/tippy.js
@@ -1,12 +1,12 @@
-tippy('#headAvatar', {
-    content: '<p class="smallText opacity-80 ml-1 mr-1"><ul><li><a href="mailto:hello@tonybermea.dev">📧 Hello@tonybermea.dev</a></li><li><a href="tel:6729998984">📞  (+1) 672.999.8984</a></li><li><a href="https://github.com/TonyBermea"><i class="fa-brands fa-github"></i> Github</a></li></ul></p>',
-    animation: 'scale',
-    placement: 'bottom',
-    theme: 'ws',
-    inertia: true,
-    interactive: true,
-    allowHTML: true,
-    followCursor: false,
-    arrow: false
+tippy("#headAvatar", {
+  content:
+    '<p class="smallText opacity-80 ml-1 mr-1"><ul><li><a href="mailto:hello@tonybermea.dev">📧 Hello@tonybermea.dev</a></li><li><a href="tel:2369711532">📞  (+1) 236.971.1532</a></li><li><a href="https://github.com/TonyBermea"><i class="fa-brands fa-github"></i> Github</a></li></ul></p>',
+  animation: "scale",
+  placement: "bottom",
+  theme: "ws",
+  inertia: true,
+  interactive: true,
+  allowHTML: true,
+  followCursor: false,
+  arrow: false,
 });
-
